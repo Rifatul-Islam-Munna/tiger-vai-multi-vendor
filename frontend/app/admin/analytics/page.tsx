c775bd4c3636794dc2c818f4e141e2144b4e0d23
Rifatul-Islam-Munna <islamrifat117@gmail.com>
2025-11-23 19:20:43 +0600

    updated frontned

--- frontend/app/admin/analytics/page.tsx
+++ frontend/app/admin/analytics/page.tsx
@@ -1,21 +1,10 @@
 "use client";
-import dynamic from "next/dynamic";
 
 import ComingSoonPage from "@/components/ui/custom/common/Comming-Soon";
 import React from "react";
 
-const Editor = dynamic(
-  () => import("@/components/ui/custom/addProduct/Description"),
-  { ssr: false }
-);
-
 const page = () => {
-  return (
-    <div>
-      <h1>My Rich Text Editor</h1>
-      <Editor />
-    </div>
-  );
+  return <ComingSoonPage />;
 };
 
 export default page;
