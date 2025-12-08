9b1ce0cca222b22e4f213c8e1f13331bbecec3b0
Rifatul-Islam-Munna <islamrifat117@gmail.com>
2025-12-08 11:21:05 +0600

    reduce the loginicon size

--- frontend/components/ui/custom/navbar/common/UserProfileDropdown.tsx
+++ frontend/components/ui/custom/navbar/common/UserProfileDropdown.tsx
@@ -50,9 +50,9 @@ export const UserProfileDropdown: React.FC<UserProfileDropdownProps> = ({
     return (
       <button
         onClick={onLoginClick}
-        className="flex items-center gap-2 bg-[var(--palette-btn)] px-2.5 md:px-4 py-2 text-xs md:text-sm rounded-lg font-medium text-white transition-all hover:shadow-lg hover:-translate-y-0.5"
+        className="flex items-center gap-1 sm:gap-2 bg-[var(--palette-btn)] px-2.5 md:px-4 py-2 text-xs md:text-sm rounded-lg font-medium text-white transition-all hover:shadow-lg hover:-translate-y-0.5"
       >
-        <User size={18} />
+        <User className=" size-3.5 sm:size-4" />
         Login
       </button>
     );
